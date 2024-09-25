dec2280ce3222ef77669398673d58ba563a6f7de
Iryna S. <78602594+au-rinko@users.noreply.github.com>
2024-09-25 13:39:05 +0300

    solve cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,11 +3,13 @@ import { env } from 'node:process';
 const parseEnv = () => {
     env.RSS_name1 = 'value1';
     const array = Object.entries(process.env);
+    let result = '';
     for (let [key, value] of array) {
         if (key.startsWith('RSS_')) {
-            console.log(`${key} = ${value};`);
+            result = `${key} = ${value};`;
         }
     }
+    console.log(result.slice(0, -1));
 };
 
 parseEnv();
