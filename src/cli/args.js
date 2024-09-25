81bf3f02a123c053d3aca547ec37d19564080ed8
Iryna S. <78602594+au-rinko@users.noreply.github.com>
2024-09-25 13:31:30 +0300

    solve cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,20 @@
 import { argv } from 'node:process';
 
 const parseArgs = () => {
-    argv.forEach((val, index) => {
-        console.log(`${index} is ${val}`);
-    });
+    let result = '';
+    for (let i = 2; i < argv.length - 1; i++) {
+        if (i % 2 === 0) {
+            const propName = argv[i].slice(2);
+            if (i === argv.length - 2) {
+                result += `${propName} is ${argv[i + 1]}`;
+            } else {
+                result += `${propName} is ${argv[i + 1]}, `;
+            }      
+        } else {
+            continue;
+        }
+    }
+    console.log(result);
 };
 
 parseArgs();
